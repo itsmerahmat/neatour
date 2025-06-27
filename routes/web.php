74ad42567d464a04e2d79bfb072f8ca8ec14b5ed
itsmerahmat <rahmatgamming1@gmail.com>
2025-06-27 11:25:36 +0800

    fix: Update redirect route in store method to point to dashboard and refine dashboard access logic

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,6 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\DestinationController;
 use App\Http\Controllers\TestimonialController;
 use App\Http\Controllers\HomeController;
-use App\Http\Controllers\ImageKitController;
-use App\Http\Controllers\Api\ImageUploadController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -15,7 +13,12 @@ Route::get('/katalog', [HomeController::class, 'katalog'])->name('katalog');
 Route::get('/katalog/{id}', [HomeController::class, 'show'])->name('katalog.detail');
 
 Route::get('dashboard', function () {
-    return Inertia::render('Dashboard');
+    $user = auth()->user();
+    if ($user->role === 'admin') {
+        return redirect()->route('destination.index');
+    } elseif ($user->role === 'superadmin') {
+        return redirect()->route('user.index');
+    }
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware(['auth', 'verified', 'role:superadmin'])->group(function () {
@@ -35,25 +38,6 @@ Route::resource('destination', DestinationController::class)->middleware(['auth'
 
 // Testimonial routes
 Route::resource('testimonial', TestimonialController::class)->middleware(['auth']);
-
-// ImageKit API routes
-Route::middleware(['auth'])->prefix('api/imagekit')->name('imagekit.')->group(function () {
-    Route::post('/upload', [ImageKitController::class, 'upload'])->name('upload');
-    Route::post('/generate-url', [ImageKitController::class, 'generateUrl'])->name('generate.url');
-    Route::post('/generate-signed-url', [ImageKitController::class, 'generateSignedUrl'])->name('generate.signed.url');
-    Route::get('/auth-params', [ImageKitController::class, 'getAuthParams'])->name('auth.params');
-    Route::delete('/delete-file', [ImageKitController::class, 'deleteFile'])->name('delete.file');
-    Route::get('/file-details', [ImageKitController::class, 'getFileDetails'])->name('file.details');
-    Route::get('/list-files', [ImageKitController::class, 'listFiles'])->name('list.files');
-    Route::post('/phash-distance', [ImageKitController::class, 'pHashDistance'])->name('phash.distance');
-});
-
-// Image Upload API routes for destinations
-Route::middleware(['auth'])->prefix('api/upload')->name('api.upload.')->group(function () {
-    Route::post('/destination-image', [ImageUploadController::class, 'uploadDestinationImage'])->name('destination.image');
-    Route::delete('/delete-image', [ImageUploadController::class, 'deleteImage'])->name('delete.image');
-    Route::post('/optimized-url', [ImageUploadController::class, 'getOptimizedUrl'])->name('optimized.url');
-});
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
